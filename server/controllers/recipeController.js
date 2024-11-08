6b26875edda08ed7d94638373dc62a01d4322da3
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2024-11-08 13:35:10 -0500

    created the categories page for the View All link

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -9,47 +9,75 @@ const Category = require('../models/CategoryModel')
  */
 
 exports.homepage = async (req, res) => {
-    res.render('index', { title: 'Cooking Blog - Home' });
-};
-
-
-async function insertDummyCategoryData() {
     try {
-        await Category.insertMany([
-            {
-                "name":"Thai",
-                "image": "thai-food.jpg"
-            },
-            {
-                "name":"American",
-                "image": "american-food.jpg"
-            },
-            {
-                "name":"Italian",
-                "image": "italian-food.jpg"
-            },
-            {
-                "name":"Mexican",
-                "image": "mexican-food.jpg"
-            },
-            {
-                "name":"Indian",
-                "image": "thindianai-food.jpg"
-            },
-            {
-                "name":"Chinese",
-                "image": "chinese-food.jpg"
-            },
-            {
-                "name":"Korean",
-                "image": "korean-food.jpg"
-            },
-        ]);
-
+        
+        const limitNumber = 5;
+        const categories = await Category.find({}).limit(limitNumber);
+        res.render('index', { title: 'Cooking Blog - Home', categories });
     } catch (error) {
-        console.log('err', + error);
+        console.log('err', error);
+        res.status(500).send({msg: error.message || 'Error Occurred'})
     }
 };
 
-insertDummyCategoryData();
+
+/**
+ * Get /categories
+ * Explore Categories
+ */
+
+exports.exploreCategories = async (req, res) => {
+    try {
+        
+        const limitNumber = 20;
+        const categories = await Category.find({}).limit(limitNumber);
+        res.render('categories', { title: 'Cooking Blog - Explore Categories', categories });
+    } catch (error) {
+        console.log('err', error);
+        res.status(500).send({msg: error.message || 'Error Occurred'})
+    }
+};
+
+
+
+
+// async function insertDummyCategoryData() {
+//     try {
+//         await Category.insertMany([
+//             {
+//                 "name":"Thai",
+//                 "image": "thai-food.jpg"
+//             },
+//             {
+//                 "name":"American",
+//                 "image": "american-food.jpg"
+//             },
+//             {
+//                 "name":"Italian",
+//                 "image": "italian-food.jpg"
+//             },
+//             {
+//                 "name":"Mexican",
+//                 "image": "mexican-food.jpg"
+//             },
+//             {
+//                 "name":"Indian",
+//                 "image": "thindianai-food.jpg"
+//             },
+//             {
+//                 "name":"Chinese",
+//                 "image": "chinese-food.jpg"
+//             },
+//             {
+//                 "name":"Korean",
+//                 "image": "korean-food.jpg"
+//             },
+//         ]);
+
+//     } catch (error) {
+//         console.log('err', + error);
+//     }
+// };
+
+// insertDummyCategoryData();
 
